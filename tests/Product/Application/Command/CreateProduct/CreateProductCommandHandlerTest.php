5cb6087ed85a42f9d2cded23b6073a29fd600c70
Igor Nemykin <webroru@gmail.com>
2025-11-23 13:18:19 +0100

    update CreateProductCommandHandlerTest

--- tests/Product/Application/Command/CreateProduct/CreateProductCommandHandlerTest.php
+++ tests/Product/Application/Command/CreateProduct/CreateProductCommandHandlerTest.php
@@ -7,6 +7,7 @@ namespace App\Tests\Product\Application\Command\CreateProduct;
 use App\Application\Command\CreateProduct\CreateProductCommand;
 use App\Application\Command\CreateProduct\CreateProductCommandHandler;
 use App\Domain\Entity\ProductInterface;
+use App\Domain\Factory\ProductFactoryInterface;
 use App\Domain\Repository\ProductRepositoryInterface;
 use PHPUnit\Framework\TestCase;
 
@@ -15,13 +16,14 @@ class CreateProductCommandHandlerTest extends TestCase
     public function testItCreatesProductAndSavesToRepository(): void
     {
         $repository = $this->createMock(ProductRepositoryInterface::class);
+        $factory = $this->createMock(ProductFactoryInterface::class);
 
         $repository
             ->expects(self::once())
             ->method('save')
             ->with(self::isInstanceOf(ProductInterface::class));
 
-        $handler = new CreateProductCommandHandler($repository);
+        $handler = new CreateProductCommandHandler($repository, $factory);
 
         $command = new CreateProductCommand(
             name: 'Test',
