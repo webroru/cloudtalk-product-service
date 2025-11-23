bf533d415b0284c2be13522dd0d2d6a0e8d46b97
Igor Nemykin <webroru@gmail.com>
2025-11-23 18:25:15 +0100

    add Product domain entity

---
+++ src/Domain/Entity/Product.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Entity;
+
+class Product implements ProductInterface
+{
+    private string $id;
+    private string $name;
+    private string $description;
+    private float $price;
+
+    public function __construct(string $id, string $name, ?string $description)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    public function getAverage(): float
+    {
+        // TODO: Implement getAverage() method.
+    }
+}
