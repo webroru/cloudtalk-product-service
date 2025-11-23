9c26de4d787fff8d7a551b79a6288704c473f2d6
Igor Nemykin <webroru@gmail.com>
2025-11-23 14:19:40 +0100

    fixup ProductInterface.php

--- src/Domain/Entity/ProductInterface.php
+++ src/Domain/Entity/ProductInterface.php
@@ -9,11 +9,10 @@ use Symfony\Component\Uid\Uuid;
 interface ProductInterface
 {
     public function getName(): Uuid;
-    public function setName(): self;
+    public function setName(string $name): self;
     public function getDescription(): string;
-    public function setDescription(): self;
+    public function setDescription(string $description): self;
     public function getPrice(): float;
-    public function setPrice(): self;
+    public function setPrice(float $price): self;
     public function getAverage(): float;
-    public function setAverage(): self;
 }
