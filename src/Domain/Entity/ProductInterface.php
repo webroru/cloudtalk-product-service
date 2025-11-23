04c53ae1b6a8a3ae151c7114d3c5f839e513270c
Igor Nemykin <webroru@gmail.com>
2025-11-23 18:26:37 +0100

    fixup ProductInterface

--- src/Domain/Entity/ProductInterface.php
+++ src/Domain/Entity/ProductInterface.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace App\Domain\Entity;
 
-use Symfony\Component\Uid\Uuid;
-
 interface ProductInterface
 {
-    public function getName(): Uuid;
+    public function getId(): string;
+    public function getName(): string;
     public function setName(string $name): self;
     public function getDescription(): string;
     public function setDescription(string $description): self;
