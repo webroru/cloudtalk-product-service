79ea8c8eff3abae7f4ea892699bb7a8a03800125
Igor Nemykin <webroru@gmail.com>
2025-11-26 12:16:03 +0100

    add ORM ProductIdType

---
+++ src/Infrastructure/Orm/ProductIdType.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Orm;
+
+use App\Domain\ValueObject\ProductId;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\GuidType;
+
+class ProductIdType extends GuidType
+{
+    public function getName() : string
+    {
+        return self::class;
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform) : ProductId
+    {
+        return ProductId::fromString($value);
+    }
+
+    /**
+     * @param ProductId $value
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform) : ?string
+    {
+        return $value?->toString();
+    }
+}
