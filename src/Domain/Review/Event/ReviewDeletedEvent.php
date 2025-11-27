56c7059963ed4692acdc0fb3f2767f384d22e176
Igor Nemykin <webroru@gmail.com>
2025-11-27 19:02:29 +0100

    add DeleteReview Command, Handler and Event

---
+++ src/Domain/Review/Event/ReviewDeletedEvent.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Review\Event;
+
+use App\Domain\Review\ValueObject\Rating;
+use App\Domain\Review\ValueObject\ReviewId;
+use App\Domain\Product\ValueObject\ProductId;
+
+final readonly class ReviewDeletedEvent
+{
+    public function __construct(
+        public ReviewId $reviewId,
+        public ProductId $productId,
+        public Rating $rating,
+    ) {
+    }
+}
