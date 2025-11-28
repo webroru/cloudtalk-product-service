fae88d3e14a62a2d435ce33cb9096fc4109d1b0e
Igor Nemykin <igor.nemykin@dhl.com>
2025-11-28 14:36:55 +0100

    add GetProductReviews Query, Handler, Response and DTO

---
+++ src/Application/Review/Query/DTO/ReviewDto.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Review\Query\DTO;
+
+final readonly class ReviewDto
+{
+    public function __construct(
+        public string $id,
+        public string $firstName,
+        public string $lastName,
+        public string $text,
+        public int $rating,
+    ) {
+    }
+}
---
+++ src/Application/Review/Query/GetProductReviews/GetProductReviewsQuery.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Review\Query\GetProductReviews;
+
+use App\Application\Shared\Bus\Query\QueryInterface;
+use App\Domain\Product\ValueObject\ProductId;
+
+final readonly class GetProductReviewsQuery implements QueryInterface
+{
+    public function __construct(
+        public ProductId $productId,
+    ) {
+    }
+}
