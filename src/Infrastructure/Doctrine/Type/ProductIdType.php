a1029ef736b96b4e2f76e43d85b8c2797820ce01
Igor Nemykin <igor.nemykin@dhl.com>
2025-11-28 15:45:05 +0100

    fixup rename Orm directory to Doctrine

--- src/Infrastructure/Doctrine/Type/ProductIdType.php
+++ src/Infrastructure/Doctrine/Type/ProductIdType.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Infrastructure\Orm;
+namespace App\Infrastructure\Doctrine\Type;
 
 use App\Domain\Product\ValueObject\ProductId;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
@@ -10,12 +10,12 @@ use Doctrine\DBAL\Types\GuidType;
 
 class ProductIdType extends GuidType
 {
-    public function getName() : string
+    public function getName(): string
     {
         return self::class;
     }
 
-    public function convertToPHPValue($value, AbstractPlatform $platform) : ProductId
+    public function convertToPHPValue($value, AbstractPlatform $platform): ProductId
     {
         return ProductId::fromString($value);
     }
@@ -23,7 +23,7 @@ class ProductIdType extends GuidType
     /**
      * @param ProductId $value
      */
-    public function convertToDatabaseValue($value, AbstractPlatform $platform) : ?string
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
     {
         return $value?->toString();
     }
