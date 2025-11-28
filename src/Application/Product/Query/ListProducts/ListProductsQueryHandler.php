63e2387b905265dd09eff868450264a5502e8f95
Igor Nemykin <igor.nemykin@dhl.com>
2025-11-28 15:33:08 +0100

    refactoring ListProductsQueryHandler

--- src/Application/Product/Query/ListProducts/ListProductsQueryHandler.php
+++ src/Application/Product/Query/ListProducts/ListProductsQueryHandler.php
@@ -6,7 +6,7 @@ namespace App\Application\Product\Query\ListProducts;
 
 use App\Application\Product\Query\DTO\ProductDto;
 use App\Application\Shared\Bus\Query\QueryHandlerInterface;
-use App\Domain\Product\Entity\Product;
+use App\Domain\Product\Entity\ProductInterface;
 use App\Domain\Product\Repository\ProductRepositoryInterface;
 
 final readonly class ListProductsQueryHandler implements QueryHandlerInterface
@@ -21,7 +21,7 @@ final readonly class ListProductsQueryHandler implements QueryHandlerInterface
         $products = $this->repository->findAll();
 
         $dtos = array_map(
-            fn(Product $product) => new ProductDto(
+            fn(ProductInterface $product) => new ProductDto(
                 id: $product->getId()->toString(),
                 name: $product->getName(),
                 price: $product->getPrice(),
