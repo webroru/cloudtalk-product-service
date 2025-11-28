3a081e1eb133c2546d775f6524cbbd1c359e5a7b
Igor Nemykin <igor.nemykin@dhl.com>
2025-11-28 17:10:07 +0100

    add DoctrineProductRepository

---
+++ src/Infrastructure/Doctrine/Repository/DoctrineProductRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Doctrine\Repository;
+
+use App\Domain\Product\Entity\Product;
+use App\Domain\Product\Entity\ProductInterface;
+use App\Domain\Product\Repository\ProductRepositoryInterface;
+use App\Domain\Product\ValueObject\ProductId;
+use Doctrine\ORM\EntityManagerInterface;
+
+final readonly class DoctrineProductRepository implements ProductRepositoryInterface
+{
+    public function __construct(
+        private EntityManagerInterface $em
+    ) {
+    }
+
+    public function save(ProductInterface $product): void
+    {
+        $this->em->persist($product);
+        $this->em->flush();
+    }
+
+    public function remove(ProductInterface $product): void
+    {
+        $this->em->remove($product);
+        $this->em->flush();
+    }
+
+    public function findById(ProductId $id): ProductInterface
+    {
+        $product = $this->em->getRepository(Product::class)->find($id->toString());
+
+        if (!$product instanceof ProductInterface) {
+            throw new \RuntimeException(sprintf('Product with id "%s" not found', $id->toString()));
+        }
+
+        return $product;
+    }
+
+    /** @return ProductInterface[] */
+    public function findAll(): array
+    {
+        return $this->em->getRepository(Product::class)->findAll();
+    }
+}
