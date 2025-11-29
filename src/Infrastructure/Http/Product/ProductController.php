a1b1a7b315bc98518ab2e6c7f6fdc096854fe591
Igor Nemykin <webroru@gmail.com>
2025-11-29 14:09:13 +0100

    add update, delete, get and list product endpoint

--- src/Infrastructure/Http/Product/ProductController.php
+++ src/Infrastructure/Http/Product/ProductController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Http\Product;
 
+use App\Domain\Product\ValueObject\ProductId;
 use App\Infrastructure\Service\ProductService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -27,5 +28,46 @@ final class ProductController extends AbstractController
         );
 
         return new JsonResponse(['status' => 'ok'], 201);
+    }
+
+    #[Route('/products/{id}', name: 'product_update', methods: ['PUT'])]
+    public function update(
+        string $id,
+        Request $request,
+    ): JsonResponse {
+        $data = json_decode($request->getContent(), true);
+
+        $this->productService->updateProduct(
+            id: ProductId::fromString($id),
+            name: $data['name'],
+            description: $data['description'] ?? null,
+            price: (float)$data['price'],
+        );
+
+        return $this->json(['status' => 'ok']);
+    }
+
+    #[Route('/products/{id}', name: 'product_delete', methods: ['DELETE'])]
+    public function delete(
+        string $id,
+    ): JsonResponse {
+        $this->productService->deleteProduct(
+            id: ProductId::fromString($id)
+        );
+
+        return $this->json(['status' => 'deleted']);
+    }
+
+    #[Route('/products/{id}', name: 'product_get', methods: ['GET'])]
+    public function get(
+        string $id,
+    ): JsonResponse {
+        return $this->json($this->productService->getProduct(ProductId::fromString($id)));
+    }
+
+    #[Route('/products', name: 'product_list', methods: ['GET'])]
+    public function list(
+    ): JsonResponse {
+        return $this->json($this->productService->listProducts());
     }
 }
--- src/Infrastructure/Service/ProductService.php
+++ src/Infrastructure/Service/ProductService.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 namespace App\Infrastructure\Service;
 
 use App\Application\Product\Command\CreateProduct\CreateProductCommand;
+use App\Application\Product\Command\DeleteProduct\DeleteProductCommand;
+use App\Application\Product\Command\UpdateProduct\UpdateProductCommand;
+use App\Application\Product\Query\GetProductById\GetProductByIdQuery;
+use App\Application\Product\Query\ListProducts\ListProductsQuery;
+use App\Domain\Product\Entity\Product;
+use App\Domain\Product\ValueObject\ProductId;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 final readonly class ProductService
@@ -15,5 +21,36 @@ final readonly class ProductService
     {
         $command = new CreateProductCommand($name, $description, $price);
         $this->commandBus->dispatch($command);
+    }
+
+    public function updateProduct(ProductId $id, string $name, string $description, float $price): void
+    {
+        $command = new UpdateProductCommand(
+            id: $id,
+            name: $name,
+            description: $description,
+            price: $price
+        );
+        $this->commandBus->dispatch($command);
+    }
+
+    public function deleteProduct(ProductId $id): void
+    {
+        $command = new DeleteProductCommand(id: $id);
+        $this->commandBus->dispatch($command);
+    }
+
+    public function getProduct(ProductId $id): Product
+    {
+        $command = new GetProductByIdQuery(id: $id);
+        $envelope = $this->commandBus->dispatch($command);
+        return $envelope->last(MessageBusInterface::class)->getResult();
+    }
+
+    public function listProducts(): array
+    {
+        $command = new ListProductsQuery();
+        $envelope = $this->commandBus->dispatch($command);
+        return $envelope->last(MessageBusInterface::class)->getResult();
     }
 }
